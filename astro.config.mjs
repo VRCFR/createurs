79b78df9acc15d1c9e580a71c80a7fd57905a665
Ranily <raphael.herold@rherold.fr>
2023-07-10 06:11:31 +0200

    Configuration des pages du tutos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,50 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
 import markdoc from "@astrojs/markdoc";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'VRChat France Créateurs',
-    social: {
-      discord: 'https://discord.gg/vrchatfrance'
-    },
-    sidebar: [{
-      label: 'Pour commencer',
-      autogenerate: {
-        directory: 'guides'
-      }
-    }, {
-      label: 'SDK',
-      autogenerate: {
-        directory: 'sdk'
-      }
-    }, {
-      label: 'Avatars',
-      autogenerate: {
-        directory: 'avatars'
-      }
-    },{
-      label:'Mondes',
-      autogenerate:{
-        directory: 'worlds'
-      }
-    },{
-      label:'Plateforme',
-      autogenerate:{
-        directory: 'platform'
-      }
-    }
-    ]
-  }), markdoc()],
-  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  integrations: [
+    starlight({
+      title: 'VRChat France Créateurs',
+      social: {
+        discord: 'https://discord.gg/vrchatfrance'
+      },
+      sidebar: [
+        {
+          label: 'Pour commencer',
+          autogenerate: { directory: 'guides' }
+        },
+        {
+          label: 'SDK',
+          autogenerate: {
+            directory: 'sdk',
+            collapsed: true
+          }
+        },
+        {
+          label: 'Avatars',
+          autogenerate: {
+            directory: 'avatars'
+          }
+        },
+        {
+          label: 'Mondes',
+          autogenerate: {
+            directory: 'worlds'
+          }
+        },
+        {
+          label: 'Plateforme',
+          autogenerate: {
+            directory: 'platform'
+          }
+        }
+      ]
+    }), 
+    markdoc(), 
+    sitemap()
+  ],
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp'
