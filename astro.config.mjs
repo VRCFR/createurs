3ecc5c48891e3735587dbb8a7ad3e710d2819afd
Ranily <raphael.herold@rherold.fr>
2023-08-30 19:41:33 +0200

    Update du Astro Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,10 +33,8 @@ export default defineConfig({
         },
         {
           label: 'SDK',
-          collapsed: true,
           autogenerate: {
-            directory: 'SDK',
-            collapsed: true,
+            directory: 'sdk',
           }
         },
         {
