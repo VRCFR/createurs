2307decd78df954c0faf30cc6f561ed9bef75a4e
Ranily <raphael.herold@rherold.fr>
2023-09-11 07:28:35 +0200

    📝 Correction mineur de faute de markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,7 +35,7 @@ export default defineConfig({
           collapsed: true,
           autogenerate: {
             collapsed: true,
-            directory: 'SDK',
+            directory: 'sdk',
           }
         },
         {
